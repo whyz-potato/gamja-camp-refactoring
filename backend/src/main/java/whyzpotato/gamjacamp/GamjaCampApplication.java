57a2e8e6669d5fc75faa63b079ea89fef69616df
Daseul <daseull.rep@gmail.com>
2023-04-22 18:32:46 +0900

    dev : 프로젝트 생성

---
+++ backend/src/main/java/whyzpotato/gamjacamp/GamjaCampApplication.java
@@ -0,0 +1,13 @@
+package whyzpotato.gamjacamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamjaCampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamjaCampApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/whyzpotato/gamjacamp/GamjaCampApplicationTests.java
@@ -0,0 +1,13 @@
+package whyzpotato.gamjacamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamjaCampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
