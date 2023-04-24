80be19ef436fb4a7275d4c0b69d05bc60efa5a2b
jaqkqh15 <jaqkqh15@naver.com>
2023-04-24 23:30:31 +0900

    Naver Map init +geolocation

---
+++ frontend/src/components/Map.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <div id="map" ref="maps" style="width:80%; height:700px;"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      lat: null,
+      lng: null
+    }
+  },
+  mounted () {
+    navigator.geolocation.getCurrentPosition((e) => {
+      this.lat = e.coords.latitude
+      this.lng = e.coords.longitude
+      console.log(this.lat, this.lng)
+
+      const map = new window.naver.maps.Map('map',{
+        center: new window.naver.maps.LatLng(this.lat, this.lng),
+        zoom: 15
+      })
+
+      new window.naver.maps.Marker({
+        position: new window.naver.maps.LatLng(this.lat, this.lng),
+        map: map
+      })
+    })
+
+
+    
+  },
+  methods: {
+  }
+}
+</script>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,35 +1,27 @@
 <template>
   <div>
-    <v-btn @click="getMyPosition">
-      위치확인
-    </v-btn>
+    <Map></Map>
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'Home',
-    data () {
-      return {
-        lat: null,
-        lng: null
-      }
-    },
-    mounted () {
- 
-    },
-    methods: {
-      getMyPosition () {
-        navigator.geolocation.getCurrentPosition(
-          this.getPosition
-        )
-      },
-      getPosition (val) {
-        console.log(val)
-        this.lat = val.coords.latitude
-        this.lng = val.coords.longitude
-        console.log(this.lat, this.lng)
-      }
+import Map from '@/components/Map'
+
+export default {
+  name: 'Home',
+  components: {
+    Map
+  },
+  data () {
+    return {
+
     }
+  },
+  mounted () {
+
+  },
+  methods: {
+
   }
+}
 </script>
