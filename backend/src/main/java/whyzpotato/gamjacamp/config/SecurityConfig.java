e18bc6917d8b3b17cfdb21aa278dbd6e6482ac11
Daseul <daseull.rep@gmail.com>
2023-07-16 20:32:35 +0900

    fix : oauth2 cors 문제 해결(안) 2

--- backend/src/main/java/whyzpotato/gamjacamp/config/SecurityConfig.java
+++ backend/src/main/java/whyzpotato/gamjacamp/config/SecurityConfig.java
@@ -6,6 +6,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import whyzpotato.gamjacamp.config.auth.CustomOAuth2UserService;
 import whyzpotato.gamjacamp.domain.member.Role;
 
@@ -20,7 +23,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http
-                .cors();
+                .cors().configurationSource(corsConfigurationSource());
 
         //relax csrf for sockJS
         http
@@ -57,5 +60,19 @@ public class SecurityConfig {
                 .userService(customOAuth2UserService); // 소셜로그인 성공 후 조치
 
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource(){
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.addAllowedOriginPattern("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
     }
 }
