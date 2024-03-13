b2f3a209c969a58f8fc39972d954593ff963e514
hs03130 <hs03130@ourlook.kr>
2024-03-13 12:29:54 +0900

    add: 이미지 sql

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -29,4 +29,14 @@ insert into room (camp_id, capacity, cnt, name, week_price, weekend_price, room_
 insert into room (camp_id, capacity, cnt, name, week_price, weekend_price, room_id) values (10, 10, 1, '단체1', 150000, 250000, 27);
 insert into room (camp_id, capacity, cnt, name, week_price, weekend_price, room_id) values (10, 10, 1, '단체2', 150000, 250000, 28);
 
+insert into image (image_id, file_name, path, camp_id, post_id, review_id, room_id) values (101, 'a9419d73-c13f-450f-b4c4-1a68df2de9e3.jpg' , 'https://gamja-camp.s3.ap-northeast-2.amazonaws.com/a9419d73-c13f-450f-b4c4-1a68df2de9e3.jpg'  , 2, null, null, null);
+insert into image (image_id, file_name, path, camp_id, post_id, review_id, room_id) values (102, '958395d7-92f5-4805-85a4-8401c0cc7501.jpg' , 'https://gamja-camp.s3.ap-northeast-2.amazonaws.com/958395d7-92f5-4805-85a4-8401c0cc7501.jpg'  , 2, null, null, null);
+insert into image (image_id, file_name, path, camp_id, post_id, review_id, room_id) values (103, 'e1c2d8bb-e0d9-4071-afb2-f8bfdd9bd266.jpg' , 'https://gamja-camp.s3.ap-northeast-2.amazonaws.com/e1c2d8bb-e0d9-4071-afb2-f8bfdd9bd266.jpg'  , 3, null, null, null);
+insert into image (image_id, file_name, path, camp_id, post_id, review_id, room_id) values (104, 'a61ffe75-4a8e-42a9-890d-6fd6f2c0dd96.jpg' , 'https://gamja-camp.s3.ap-northeast-2.amazonaws.com/a61ffe75-4a8e-42a9-890d-6fd6f2c0dd96.jpg'  , 4, null, null, null);
+insert into image (image_id, file_name, path, camp_id, post_id, review_id, room_id) values (105, '7e9fcacd-f50c-4c50-b136-0dc22eea5dd5.jpg' , 'https://gamja-camp.s3.ap-northeast-2.amazonaws.com/7e9fcacd-f50c-4c50-b136-0dc22eea5dd5.jpg'  , 5, null, null, null);
+insert into image (image_id, file_name, path, camp_id, post_id, review_id, room_id) values (106, '12d8f24e-d3cc-41a3-9c81-828b8d0fb093.jpg' , 'https://gamja-camp.s3.ap-northeast-2.amazonaws.com/12d8f24e-d3cc-41a3-9c81-828b8d0fb093.jpg'  , 6, null, null, null);
+insert into image (image_id, file_name, path, camp_id, post_id, review_id, room_id) values (107, '07e0e10b-4c30-45ee-ad13-723547239103.jpg' , 'https://gamja-camp.s3.ap-northeast-2.amazonaws.com/07e0e10b-4c30-45ee-ad13-723547239103.jpg'  , 7, null, null, null);
+insert into image (image_id, file_name, path, camp_id, post_id, review_id, room_id) values (108, 'a5e65053-7e6e-4c99-9aca-7bb02607573a.jpg' , 'https://gamja-camp.s3.ap-northeast-2.amazonaws.com/a5e65053-7e6e-4c99-9aca-7bb02607573a.jpg'  , 8, null, null, null);
+insert into image (image_id, file_name, path, camp_id, post_id, review_id, room_id) values (109, '8c99c62f-a700-44f9-8032-920bf9420dc3.jpg' , 'https://gamja-camp.s3.ap-northeast-2.amazonaws.com/8c99c62f-a700-44f9-8032-920bf9420dc3.jpg'  , 9, null, null, null);
+
 
